c3cbf557ec08e13609fd6a144e7d18d3166ff0eb
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-04-01 01:05:55 +0900

    flux flow with new context api

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -6,15 +6,48 @@ const ThemeContext = React.createContext();
 export class ThemeProvider extends React.Component {
   state = {
     theme: "light",
+    count: 1,
   };
 
   render() {
-    return <ThemeContext.Provider value={{ state: this.state }}>{this.props.children}</ThemeContext.Provider>;
+    return (
+      <ThemeContext.Provider
+        value={{
+          state: this.state,
+          actions: {
+            increment: () => {
+              this.setState(prevState => {
+                return { count: prevState.count + 1 };
+              });
+            },
+            decrement: () => {
+              this.setState(prevState => {
+                return { count: prevState.count - 1 };
+              });
+            },
+          },
+        }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
   }
 }
 
 export class ThemeConsumer extends React.Component {
   render() {
-    return <ThemeContext.Consumer>{context => <div>{context.state.theme}</div>}</ThemeContext.Consumer>;
+    return (
+      <ThemeContext.Consumer>
+        {value => {
+          return (
+            <div>
+              <div>{value.state.count}</div>
+              <button onClick={value.actions.increment}>+</button>
+              <button onClick={value.actions.decrement}>-</button>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
   }
 }
